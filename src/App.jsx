af3dcfecf6398356cdbbbfeb9b40738452f74f83
amirthavalli1987 <amirtha.dit@gmail.com>
2024-12-07 20:29:51 +0530

    api added to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/auth/register" element={<Registration />}></Route>
         <Route path="/auth/login" element={<Login />}></Route>
-        <Route path="/pet/create-pets" element={<CreatePets />}></Route>
+        <Route path="/api/pet/create-pets" element={<CreatePets />}></Route>
         <Route path="/pet/saved-pets" element={<SavedPets />}></Route>
         <Route path="/read-pet/:id" element={<Readpet />}></Route>
       </Routes>
