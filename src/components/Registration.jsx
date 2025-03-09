180cfceba28a928071eed9733df436207bc98534
amirthavalli1987 <amirtha.dit@gmail.com>
2025-03-09 16:28:03 +0530

    d

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -6,21 +6,36 @@ import{Link,useNavigate} from 'react-router-dom'
 
 const Registration = () => {
     const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
   const navigate = useNavigate()
   
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios
-          .post("https://backend-capstone-8-81l3.onrender.com/auth/register", {
-            username,
-            password,
-          })
-          .then((result) => {
-            navigate("/auth/login");
-            console.log(result);
-          })
-          .catch((err) => console.log(err));
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      
+    if (!username || !password) {
+      setError('Both username and password are required');
+      return;
+    }
+    setLoading(true);
+    setError('');
+    axios
+      .post("https://backend-capstone-8-81l3.onrender.com/auth/register", {
+        username,
+        password,
+      })
+      .then((result) => {
+        setLoading(false);
+        navigate("/auth/login");
+        console.log(result);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError('An error occured during registration.please try again');
+        console.log(err);
+      });
     }
     
 
@@ -28,6 +43,7 @@ const Registration = () => {
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div className="p-3 border border-1 w-25">
         <h3>Register</h3>
+        {error && <div className='alert aler-danger'>{error}</div>}
         <form onSubmit={handleSubmit}>
           <div>
             <label>Username</label>
@@ -46,7 +62,7 @@ const Registration = () => {
               onChange={(e) => setPassword(e.target.value)}
             ></input>
           </div>
-          <button className="mt-1 btn btn-success w-100">Submit</button>
+          <button className="mt-1 btn btn-success w-100" disabled={loading}>{loading ? 'Registering...' : 'Submit'}</button>
       <Link to="/auth/login"><button className="btn btn-default w-100 mt-2">Login</button></Link>
           
         </form>
